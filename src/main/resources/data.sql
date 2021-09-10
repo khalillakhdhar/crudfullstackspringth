d2107de710a16940452533dc3bd4ddf93194ade9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-10 10:21:51 +0100

    page

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into student values(1, 'Computer Science', 'khalil lakhdhar', 'The Tech Pro', '2019-06-27');
-insert into student values(2, 'Managment', 'Mohamed yaakoubi',  'The Tech Pro', '2019-06-27');
-insert into student values(3, 'Data Science', 'Solace Okeke', 'The Tech Pro', '2019-06-27');
-insert into student values(4, 'Computer Science', 'Barack Obama',  'The Tech Pro', '2019-06-27');
-insert into student values(5, 'US President', 'Donald Trump',  'The Tech Pro', '2019-06-27');
