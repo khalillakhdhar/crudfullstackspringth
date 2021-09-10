c9fd3336b59539fe0f1bb7e782011326fab3c0de
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-10 11:00:22 +0100

    serveur

---
+++ src/main/java/com/thym/heroku/controller/StudentController.java
@@ -0,0 +1,48 @@
+package com.thym.heroku.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.thym.heroku.models.Student;
+import com.thym.heroku.service.StudentService;
+
+@Controller
+@RequestMapping("/students")
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+	@RequestMapping("getAll")
+	public String getAll(Model model)
+	{
+		List<Student> students = studentService.getAll();
+		model.addAttribute("students",students);
+		return "students";
+	}
+	@PostMapping(value="/addNew")
+	public String addNew(Student student)
+	{
+		studentService.addNew(student);
+		return "redirect:/students/getAll";
+	}
+	@RequestMapping("/getOne")
+	@ResponseBody
+	public Optional<Student> getOne(Integer Id)
+	{
+		return studentService.getOne(Id);
+	}
+	@RequestMapping(value="/delete", method= {RequestMethod.DELETE,RequestMethod.GET})
+	public String delete(Integer Id)
+	{
+		studentService.delete(Id);
+		return "redirect:/students/getAll";
+	}
+}
